75b10c5e484782177030e4e164b375a2bc8d7adc
weeclicks <weeclicks@Kais-Mac-mini.local>
2018-02-07 16:24:39 +0800

    webkit listener

--- js/game_manager.js
+++ js/game_manager.js
@@ -84,7 +84,6 @@ GameManager.prototype.actuate = function () {
   // Clear the state when the game is over (game over only, not win)
   if (this.over) {
     this.storageManager.clearGameState();
-    window.webkit.messageHandlers.score.postMessage(this.storageManager.getBestScore());
   } else {
     this.storageManager.setGameState(this.serialize());
   }
